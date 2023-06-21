1762da581a917f5aa1f44df5169705341eabc458
张泽宇 <fearless@192.168.1.104>
2023-06-22 00:36:25 +0800

    commit for this update

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,18 +1,38 @@
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
 import React, { useState } from 'react';
-import { Layout, message } from 'antd';
+import { Layout, Menu, message } from 'antd';
 import { logout } from '../utils';
 import PageHeader from './Pageheader';
 
-const { Header, Content } = Layout;
+
+const { Header, Content, Sider } = Layout;
+
+function getItem(label, key, icon) {
+  return {
+    key,
+    icon,
+    label,
+  };
+}
+const items = [
+  getItem('Order', '1', <PieChartOutlined />),
+  getItem('Current order', '2', <DesktopOutlined />),
+  getItem('Historical orders', '3', <FileOutlined />), 
+  getItem('user', '4', <UserOutlined />),
+];
 
 function Homepage() {
   const [loggedIn, setLoggedIn] = useState(false)
-
+  const [collapsed, setCollapsed] = useState(false);
 
   const signinOnSuccess = () => {
     setLoggedIn(true);
   }
-
 
   const signoutOnClick = () => {
     logout().then(() => {
@@ -33,6 +53,10 @@ function Homepage() {
         />
         </Header>
       <Layout>
+      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      </Sider>
         <Layout style={{ padding: '24px' }}>
           <Content
             className="site-layout-background"
